500b6a5fb3ab6e1c8b28ca7e907a7204ed5f647c
Taylor <taylor.r.meador@gmail.com>
2026-01-17 16:19:04 -0700

    prepare for chart building

--- return_models/monte_carlo.py
+++ return_models/monte_carlo.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from historical_bootstrap import bootstrap_returns
 
 
@@ -7,12 +9,17 @@ class Simulation:
         self.inflation_rate = 0.03
         self.allocation = {"stocks": 0.6, "bonds": 0.4}
         self.withdrawal_rate = 0.04
+        self.n_samples = 10000
+        self.failures = 0
 
     def run(self):
-        for _ in range(10000):
+        all_simulation_data = []
+        for sample in range(self.n_samples):
             all_returns = bootstrap_returns()
             withdrawal = self.initial_portfolio_value * self.withdrawal_rate
             portfolio_value = self.initial_portfolio_value
+            simulation_data = []
+            success = True
             for i, returns in enumerate(all_returns):
                 portfolio_value -= withdrawal
 
@@ -20,9 +27,19 @@ class Simulation:
                 bond_gains = portfolio_value * self.allocation["bonds"] * returns[1]
 
                 portfolio_value = portfolio_value + stock_gains + bond_gains
-                print(
-                    f"year #{i + 1}: portfolio_value=${portfolio_value} stocks={returns[0]}%, ${stock_gains} bonds={returns[1]}%, ${bond_gains}\n"
-                )
+                if portfolio_value <= 0:
+                    success = False
+
+                year_data = {
+                    "year": i,
+                    "end_portfolio_value": portfolio_value,
+                    "stock_return": returns[0],
+                    "bond_return": returns[1],
+                    "withdrawal": withdrawal,
+                }
+                simulation_data.append(year_data)
+
+            final_portfolio_value = simulation_data[-1]["end_portolio_value"]
 
 
 if __name__ == "__main__":
